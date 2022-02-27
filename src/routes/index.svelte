3f779cd1d00bf740fd45c7ada9650fd1b1604c12
spacepolice10 <44314842+spacepolice10@users.noreply.github.com>
2022-02-27 16:57:16 +0300

    feat: implement page for one article

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,12 +3,15 @@
     query MyQuery {
         posts {
             nodes {
-            excerpt
             title
+            excerpt
+            uri
+            commentCount
             author {
                 node {
                 username
                 avatar {
+                    foundAvatar
                     url
                 }
                 }
@@ -47,7 +50,7 @@
 		}
 		return {
 			status: response.status,
-			error: new Error(`Could not load ${url}`)
+			error: new Error(`Could not load`)
 		};
 	}
 </script>
@@ -57,21 +60,28 @@
     console.log(posts)
 </script>
 
-<div class="grid grid-cols-1 md:grid-cols-3 gap-5 p-5 cursor-pointer">
+<div class="grid grid-cols-1 md:grid-cols-3 gap-5 p-5">
     {#each posts as post} 
-    <div class="flex flex-col">
-        <img src={post.featuredImage.node.mediaItemUrl} alt={post.featuredImage.node.altText} />
-        <div class="flex gap-2 w-5 h-5 my-2">
-            <img class="rounded-full" src={post.author.node.avatar.url} alt="author's avatar" />
-            <p>{post.author.node.username}</p>
+    <a href={`/post${post.uri}`}>
+        <div class={"flex flex-col hover:scale-105 duration-150"}>
+            <img src={post.featuredImage.node.mediaItemUrl} alt={post.featuredImage.node.altText} />
+            <div class="flex gap-2 w-5 h-5 my-2">
+                <img class="rounded-full" src={post.author.node.avatar.url} alt="author's avatar" />
+                <p>{post.author.node.username}</p>
+            </div>
+            <h1 class="font-bold text-2xl">{post.title}</h1>
+            <p>{@html post.excerpt}</p>
+            <div class="flex gap-2 opacity-50">
+                {#each post.tags.nodes as tag}
+                <p class="font-thin"># {tag.name}</p>
+                {/each}
+                <div class="flex justify-between items-center gap-2 ml-4">
+                    <svg class="w-4 h-4" fill="white" id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 122.97 122.88"><title>instagram-comment</title><path d="M61.44,0a61.46,61.46,0,0,1,54.91,89l6.44,25.74a5.83,5.83,0,0,1-7.25,7L91.62,115A61.43,61.43,0,1,1,61.44,0ZM96.63,26.25a49.78,49.78,0,1,0-9,77.52A5.83,5.83,0,0,1,92.4,103L109,107.77l-4.5-18a5.86,5.86,0,0,1,.51-4.34,49.06,49.06,0,0,0,4.62-11.58,50,50,0,0,0-13-47.62Z"/></svg>
+                    <p>{post.commentCount}</p>
+                </div>
+            </div>
         </div>
-        <h1 class="font-bold text-2xl">{post.title}</h1>
-        <p>{@html post.excerpt}</p>
-        <div class="flex gap-2 opacity-50">
-            {#each post.tags.nodes as tag}
-            <p class="font-thin"># {tag.name}</p>
-            {/each}
-        </div>
-    </div>
+    </a>
     {/each}
 </div>
+    
---
+++ src/routes/post/[slug].svelte
@@ -0,0 +1,109 @@
+<script context="module">
+	export const prerender = true;
+	const query = `
+    query MyQuery($slug: ID!) {
+  post(id: $slug, idType: SLUG) {
+    content
+    title
+    featuredImage {
+      node {
+        mediaItemUrl
+      }
+    }
+    author {
+      node {
+        nickname
+        username
+        firstName
+        lastName
+      }
+    }
+    tags {
+      nodes {
+        name
+      }
+    }
+    comments {
+      nodes {
+        content
+        author {
+          node {
+            name
+            ... on User {
+              id
+              email
+              avatar {
+                url
+              }
+            }
+          }
+        }
+      }
+    }
+    commentCount
+  }
+}
+
+
+	`;
+	export async function load({ url, params, fetch }) {
+    const response = await fetch(import.meta.env.VITE_PUBLIC_WORDPRESS_API_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ 
+      	query, 
+      	variables: {
+      		slug: params.slug
+      	}
+      }),
+    });
+	if (response.ok) {
+      const responseObj = await response.json();
+      const post = responseObj.data.post;
+			return {
+				props: {
+					post
+				}
+			};
+		}
+		return {
+			status: response.status,
+			error: new Error(`Could not load ${url}`)
+		};
+	}
+</script>
+
+<script>
+	export let post
+    console.log(post)
+</script>
+
+<div class="flex justify-center items-center w-full min-h-screen">
+    <div class="grid grid-cols-1 gap-10 w-10/12">
+        <h1 class="font-bold text-4xl">{post.title}</h1>
+        <div class="flex gap-10">
+            <img class="w-96" src={`${post.featuredImage.node.mediaItemUrl}`} alt />
+            <div class="grid grid-cols-1">
+                <p>Author: {post.author.node.firstName} {post.author.node.lastName}</p>
+                <p>Comments: {post.commentCount}</p>
+                <div class="flex gap-2 opacity-50">
+                    {#each post.tags.nodes as tag}
+                    <p class="font-thin"># {tag.name}</p>
+                    {/each}
+                </div>
+            </div>
+        </div>
+            <p>
+                {@html post.content}
+            </p>
+        {#each post.comments.nodes as comment}
+        <div class="flex">
+            <p class="flex opacity-50 font-thin"> -{@html comment.content}</p>
+        </div>
+        {/each}
+    </div>
+</div>
+
+
