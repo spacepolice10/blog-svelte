fb1b0138465344697915b4f6fc46ed717ab1886f
spacepolice10 <44314842+spacepolice10@users.noreply.github.com>
2022-02-27 13:53:54 +0300

    feat: implement basic "ping data" feature

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,40 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script context="module">
+	const query = `
+    query MyQuery {
+        posts {
+            nodes {
+                title
+                content
+            }
+        }
+    }
+
+	`;
+	export async function load({ fetch }) {
+    const response = await fetch(import.meta.env.VITE_PUBLIC_WORDPRESS_API_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ query }),
+    });
+		if (response.ok) {
+      const responseObj = await response.json(); 
+      const posts = responseObj.data.posts.nodes;
+			return {
+				props: {
+					posts
+				}
+			};
+		}
+		return {
+			status: response.status,
+			error: new Error(`Could not load ${url}`)
+		};
+	}
+</script>
+
+<script>
+    export let posts;
+    console.log(posts)
+</script>
